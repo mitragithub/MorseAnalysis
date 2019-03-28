572feaee7f7b8287b53f66601871439a4cf73af4
Dingkang Wang <1257332448@qq.com>
2019-03-28 14:16:35 -0400

    change names, update readme, workflow
    Change script names, make them consistent with gdoc.
    Update work flow, we use gaussian smoothing in triangulation, not in discrete morse.

--- run.sh
+++ run.sh
@@ -19,20 +19,20 @@ output_folder="result/${filename}"
 
 # first, convert image to a grid file, stored under output_folder
 echo "Running image2grid......"
-python image2grid.py $image_path $output_folder
+python get_pixels.py $image_path $output_folder
 
 # Then run jiayuan's triangulation code, get vert, edge, tri after removing
 # zero-value pixels. There could a third parameter (Gaussian_sigma, default=1).
 echo "Running triangulation......"
-python threshold_triangulation.py $image_path $input_folder $GAUSSIAN
+python triangulation.py $image_path $input_folder $GAUSSIAN
 
 # Run discrete morse with vert, edge, tri files
 echo "Running discrete morse......"
-python graphRecon.py $filename -t 2 $persistence_threshold
+python discrete_morse.py $filename -t 2 $persistence_threshold
 
 # Remove redundant edges of discrete morse output, in new_vert, new_edge.txt
 echo "Running RemoveBranch2D......"
-./RemoveBranch2D $output_folder $l $w $intensity_threshold
+./simplify_by_intensity $output_folder $l $w $intensity_threshold
 
 # convert new_vert, new_edge to an image
 echo "Running graph2image.py......"
@@ -40,6 +40,6 @@ python graph2image.py $output_folder $l $w
 
 # output geojson
 echo "Running geojson_2D.py......"
-# python geojson_2D.py $output_folder
+python graph2geojson.py $output_folder
 
 echo "Done:)"
